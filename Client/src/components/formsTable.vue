c0e4cc75723945eb1a36031ffcd5ff5c41115a1b
Gal <galrosenthal@gmail.com>
2020-08-25 16:53:15 +0300

    started working on adding a new form

--- Client/src/components/formsTable.vue
+++ Client/src/components/formsTable.vue
@@ -1,6 +1,10 @@
 <template>
-  <div >
-    <b-table striped hover :items="allForms">
+  <div>
+    <!--Button for creating new form -->
+    <b-button block pill variant="info" sticky-header class="new-form-btn" @click="showAddFormModal = !showAddFormModal">Add new form</b-button>
+
+    <!--Forms table showing all forms from the DB -->
+    <b-table striped hover stacked="md" :items="allForms" outlined no-border-collapse>
       <template v-slot:cell(LinkToSubmit)="data">
         <b-button variant="link" @click="openSubmitForm(data.item.id)" :key="data.item.id">Submit to this form</b-button>
       </template>
@@ -8,6 +12,9 @@
         <b-button variant="link" @click="showSubmissionModal = !showSubmissionModal" :key="data.item.id">View all Submissions of this form</b-button>
       </template>
     </b-table>
+
+    <!--Submission Modal -->
+    <!--TODO: maybe change to page-->
     <b-modal v-model="showSubmissionModal" hide-footer title="Using Component Methods">
       <div class="d-block text-center">
         <h3>Hello From Submission Modal!</h3>
@@ -15,6 +22,7 @@
       <b-button class="mt-3" variant="outline-danger" block @click="showSubmissionModal = !showSubmissionModal">Close Me</b-button>
     </b-modal>
 
+    <!--Submit Modal -->
     <b-modal v-model="showSubmitModal"
              scrollable
              centered
@@ -25,14 +33,25 @@
       <div class="d-block text-center" >
         <selected-form @form-na="closeModal" :form_id="selectedFormId">Hello From Submit Modal!</selected-form>
       </div>
-<!--      <b-button class="mt-3" variant="outline-danger" block @click="showSubmitModal = !showSubmitModal">Close Me</b-button>-->
     </b-modal>
+
+    <b-modal
+    hide-header
+    lazy
+    align="left"
+    v-model="showAddFormModal"
+    >
+    <new-form></new-form>
+    </b-modal>
+
+
   </div>
 </template>
 
 <script>
   import axios from 'axios';
   import selectedForm from '@/components/selectedForm.vue';
+  import newForm from '@/components/BuildNewForm.vue';
 
   async function getForms(url) {
     let forms
@@ -76,6 +95,7 @@
         showSubmissionModal: false,
         showSubmitModal: false,
         selectedFormId: null,
+        showAddFormModal: false
       }
     },
     methods: {
@@ -99,12 +119,15 @@
       this.updateTable();
     },
     components: {
-      selectedForm
+      selectedForm,
+      newForm
     }
 
   }
 </script>
 
 <style scoped>
-
+  .new-form-btn {
+    margin-bottom: 10px;
+  }
 </style>
